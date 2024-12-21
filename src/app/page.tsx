0eeb9c48e9910df06aba12798937870b775cdb97
naveeenmuthh <naveenmuthu05@gmail.com>
2024-12-21 12:10:19 +0530

    seo-optimizations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { BlogComponent, CoverComponent, FooterComponent, IngredientsComponent } from "./home/components";
 
 
+// Functional Components for better Code Structuring and have used best practices and static components for Better SSR and Lazy Loading
+
 export default function Home()
 {
 return <>
